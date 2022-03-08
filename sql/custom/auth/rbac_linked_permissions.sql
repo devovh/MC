2f9315416447ae1ddf23f295dc660eb92ceca710
devovh <devovh@gmail.com>
2022-03-08 06:00:02 +0100

    Core/Fix - Change text in SQL File.

--- sql/custom/auth/rbac_linked_permissions.sql
+++ sql/custom/auth/rbac_linked_permissions.sql
@@ -1,5 +1,5 @@
-DELETE FROM `rbac_linked_permissions` where `linkedId` IN ("1003","1004","1005","1006","1007");
-INSERT INTO `rbac_linked_permissions` (`id`, `linkedId`) values
+DELETE FROM `rbac_linked_permissions` WHERE `linkedId` IN ("1003","1004","1005","1006","1007");
+INSERT INTO `rbac_linked_permissions` (`id`, `linkedId`) VALUES
 ('195','1000'),
 ('195','1001'),
 ('195','1002'),
