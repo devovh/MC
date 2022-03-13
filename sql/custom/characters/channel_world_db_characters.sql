6568735978743625a6d653a82dff7f91787a07c0
devovh <devovh@gmail.com>
2022-03-13 23:05:11 +0100

    Core/DB - Fix Channel World.

--- sql/custom/characters/channel_world_db_characters.sql
+++ sql/custom/characters/channel_world_db_characters.sql
@@ -1,3 +1,3 @@
 INSERT INTO `channels`(`name`,`team`,`announce`,`ownership`,`password`,`bannedList`,`lastUsed`) VALUES 
-('world',67,1,0,'',NULL,0),
-('world',469,1,0,'',NULL,0);
+('world',67,1,0,'',NULL,1),
+('world',469,1,0,'',NULL,1);
