55f52f3be5e9bd38a7a11ad6d20ffeb2ee87da9c
devovh <devovh@gmail.com>
2022-03-14 14:27:34 +0100

    Core/DB - Fix Misc Entry in SQL Scripts.

--- sql/custom/world/npc_beastmaster.sql
+++ sql/custom/world/npc_beastmaster.sql
@@ -25,7 +25,7 @@
 -- NPC
 DELETE FROM creature_template WHERE entry = 190014;
 INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `dmgschool`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `DamageModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
-('190014','0','0','0','0','0','729','0','0','0','White Fang','BeastMaster','Speak','0','80','80','0','35','4194433','0.91','1.14286','1','0','0','2000','2000','1','1','1','0','2048','0','0','7','0','0','0','0','0','0','0','0','SmartAI','0','1','0.0125','1','1','1','1','0','0','1','0','0','2','BeastMaster','0');
+('190014','0','0','0','0','0','729','0','0','0','White Fang','Beast Master','Speak','0','80','80','0','35','4194433','0.91','1.14286','1','0','0','2000','2000','1','1','1','0','2048','0','0','7','0','0','0','0','0','0','0','0','SmartAI','0','1','0.0125','1','1','1','1','0','0','1','0','0','2','BeastMaster','0');
 
 -- NPC
 -- DELETE FROM creature_template WHERE entry = 190014;
