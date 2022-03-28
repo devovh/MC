4084014ffdedce1c070350a7baee4069af0d458d
devovh <devovh@gmail.com>
2022-03-28 23:21:24 +0200

    DB/Update - Fix Global World Channel.

--- sql/custom/characters/channel_world_db_characters.sql
+++ sql/custom/characters/channel_world_db_characters.sql
@@ -1,4 +1,4 @@
 --
 INSERT INTO `channels`(`name`,`team`,`announce`,`ownership`,`password`,`bannedList`,`lastUsed`) VALUES
-('world',67,1,0,'',NULL,1),
-('world',469,1,0,'',NULL,1);
+('world',67,1,1,'',NULL,1),
+('world',469,1,1,'',NULL,1);
